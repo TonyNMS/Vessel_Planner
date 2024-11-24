c853d7b06a3d763cfcdb0240eaade68f90ff1986
TonyNMS <j.li@ncl-marine.com>
2024-11-24 18:02:22 +0000

    Added print statement

--- src/vessel_planner/__init__.py
+++ src/vessel_planner/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 
 def create_app(test_config=None):
+    print("Creating Flask app...")
     app = Flask(__name__, instance_relative_config=True)
     
     if test_config is None:
