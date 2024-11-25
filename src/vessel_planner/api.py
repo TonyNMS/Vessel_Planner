797094fd6a50a264fa9cfa33100bcafb51b49453
TonyNMS <j.li@ncl-marine.com>
2024-11-25 16:38:59 +0000

    create a task table if it s not there

--- src/vessel_planner/api.py
+++ src/vessel_planner/api.py
@@ -30,3 +30,28 @@ def save_prop():
 
     return json.dumps("Pass") if rows else json.dumps(
         "Failed to write frame to database.")
+
+@bp.route("/check_or_create_task_table", methods=["POST"])
+@cross_origin()
+def check_or_create_task_table():
+    db = get_db()
+    try:
+       
+        result = db.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Task';").fetchone()
+        if result is None:
+           
+            db.execute("""
+                CREATE TABLE Task (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    "index" TEXT NOT NULL,
+                    columns TEXT NOT NULL,
+                    data TEXT NOT NULL
+                );
+            """)
+            db.commit()
+            return {"message": "Table `Task` created successfully"}, 201
+        else:
+            return {"message": "Table `Task` already exists"}, 200
+    except Exception as e:
+        return {"error": str(e)}, 500
+
