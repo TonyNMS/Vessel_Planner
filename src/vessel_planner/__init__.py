bde89d7066b21d25935c83ed41b2d7185cc9013b
TonyNMS <j.li@ncl-marine.com>
2024-11-25 15:58:03 +0000

    sqlschema

--- src/vessel_planner/__init__.py
+++ src/vessel_planner/__init__.py
@@ -2,7 +2,14 @@ import os
 
 from flask import Flask
 from flask_cors import CORS
-
+from flask import current_app
+import sqlite3
+def init_db():
+    db = sqlite3.connect(current_app.config["DATABASE"])
+    with open("schema.sql", mode="r") as f:
+        db.cursor().executescript(f.read())  
+    db.commit()
+    db.close()
 
 def create_app(test_config=None):
     print("Creating Flask app...")
@@ -10,8 +17,8 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     CORS(app)
     if test_config is None:
-        app.config["DATABASE"] = os.path.join(app.instance_path, "db.sqlite")
-
+        app.config.from_mapping(DATABASE=os.path.join(app.instance_path, 'db.sqlite'))
+        init_db()  
     else:
         app.config.from_mapping(test_config)
 
